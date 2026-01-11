10712d7547889ccc847fc6cc42183f31d91a112a
binaryshrey <shreyansh.saurabh01@gmail.com>
2026-01-11 07:23:17 -0500

    deployes to google cloud run

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-app = FastAPI(title="My FastAPI App")
+app = FastAPI(title="HungrAI Backend API", version="1.0.0")
 
 @app.get("/health", tags=["health"])
 def health_check():
