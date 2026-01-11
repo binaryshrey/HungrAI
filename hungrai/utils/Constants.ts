529de3b39ebb5a4608e960328e0f485b0218b922
binaryshrey <shreyansh.saurabh01@gmail.com>
2026-01-11 08:38:09 -0500

    updates prod URIs

--- hungrai/utils/Constants.ts
+++ hungrai/utils/Constants.ts
@@ -1,7 +1,8 @@
 export const GITHUB_URI = "https://github.com/binaryshrey/HungrAI";
 export const LINKEDIN_URI = "https://www.linkedin.com/in/shreyanshsaurabh/";
 export const BETTERSTACK_URI = "https://status.hungr.ai";
-export const HUNGRAI_URI = "http://localhost:3000";
+export const HUNGRAI_URI = "https://hungr-ai.vercel.app";
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://hungrai-backend-cpi3ekcjvq-uc.a.run.app";
 export const PREDICT_ENDPOINT = `${API_BASE_URL}/predict`;
